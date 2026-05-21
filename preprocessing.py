85aabf9c66263988a87361782fe5f74c3406c176
Eva-devgirl <evatsigkana@yahoo.gr>
2026-05-21 15:36:41 +0200

    added function load_greek_chunks

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-df_el = pd.read_csv("data/el-projections.tsv", sep="\t")
+def load_greek_chunks(): 
+	df_el = pd.read_csv("data/el-projections.tsv", sep="\t", header=None)
 
-df_el.columns = ["text", "labels"]
+	df_el.columns = ["text", "labels"]
 
-chunks = df_el["text"].dropna().astype(str).str.strip()
+	chunks = df_el["text"].dropna().astype(str).str.strip()
 
-chunks = chunks[chunks != ""].tolist()
+	chunks = chunks[chunks != ""].tolist()
 
+	return chunks
+	
+chunks = load_greek_chunks()
 print(len(chunks))
 print(chunks[:5])
