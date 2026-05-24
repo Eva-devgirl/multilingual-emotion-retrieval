d86ab6b0a663319b6adadc2afb383273a1a64141
Eva-devgirl <evatsigkana@yahoo.gr>
2026-05-24 11:13:53 +0200

    Add second query for retrieval testing

--- retrieval.py
+++ retrieval.py
@@ -8,7 +8,7 @@ chunks = load_greek_chunks()
 
 model = SentenceTransformer("intfloat/multilingual-e5-base")
 
-passages = ["passage: " + chunk for chunk in chunks[:100]]
+passages = ["passage: " + chunk for chunk in chunks[:1000]]
 
 embeddings = model.encode(passages, normalize_embeddings=True)
 
@@ -17,19 +17,25 @@ dimension = embeddings.shape[1]
 index = faiss.IndexFlatIP(dimension)
 index.add(embeddings)
 
-query = "query: Είμαι πολύ χαρούμενη σήμερα"
+#I am very happy today
+#I am very sad today
+queries = ["Είμαι πολύ χαρούμενη σήμερα", 
+	  "Eίμαι πολύ λυπημένη σήμερα"
+]
 
-query_embedding = model.encode([query], normalize_embeddings=True)
+query_embeddings = model.encode(queries, normalize_embeddings=True)
 
 # convert for faiss
-query_embedding = np.array(query_embedding).astype("float32")
+query_embedding = np.array(query_embeddings).astype("float32")
 
 #retrieval 
-scores, indices = index.search(query_embedding, k=5)
+scores, indices = index.search(query_embeddings, k=5)
 
-print("Query:", query)
+print("Query:", queries)
 print("\nTop 5 similar chunks:")
 
-for score, idx in zip(scores[0], indices[0]):
-	print("\nScore:", score)
-	print("Text:", chunks[idx])
+for query, query_scores, query_indices in zip(queries, scores, indices):
+	print("\nQuery:", query)
+	for score, idx in zip(query_scores, query_indices):
+		print("\nScore:", score)
+		print("Text:", chunks[idx])
